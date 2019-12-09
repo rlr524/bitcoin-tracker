b995d91934c4841d09dd7d97e711860194403443
Rob Ranf <robranf@gmail.com>
2019-12-08 16:19:46 -0800

    adding get from api

--- index.js
+++ index.js
@@ -1,10 +1,25 @@
-import express from "express";
-import bodyParser from "body-parser";
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
 
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.listen(3000, function() {
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+app.post("/", (req, res) => {
+  request(
+    "https://apiv2.bitcoinaverage.com/indices/global/ticker/BTCUSD",
+    function(error, response, body) {
+      console.log(body);
+    }
+  );
+  //   console.log(req.body.crypto);
+});
+
+app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
